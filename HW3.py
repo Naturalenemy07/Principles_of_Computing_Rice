8a898ad2e8002c0a7ef444bdd9c53e8d3de0d58e
John Caruthers <john.d.caruthers@gmail.com>
2021-01-12 04:40:23 -0500

    completed problem 9

--- HW3.py
+++ HW3.py
@@ -95,4 +95,38 @@ a_sum = 0.5 * n * (n + 1)
 exp_value = a_sum / n
 
 ## Question 9: Monte Carlo Simulations
+# A unit circle surrounded by a square.  The function only returns points that are in the unit circle, each point in the square
+# has an equal chance of being selected.
+""" 
+Program that computes mystery number
+"""
 
+
+import math
+import random
+
+def inside_unit_circle(point):
+    """
+    Compute distance of point from origin
+    """
+    distance = math.sqrt(point[0] ** 2 + point[1] ** 2)
+    return distance < 1
+                                                 
+
+def estimate_mystery(num_trials):
+    """
+    Main function
+    """
+    num_inside = 0
+    
+    for dumm_idx in range(num_trials):
+        new_point = [2 * random.random() - 1, 2 * random.random() - 1]
+        if inside_unit_circle(new_point):
+            num_inside += 1
+    
+    return float(num_inside) / num_trials
+
+print estimate_mystery(10000)
+
+# the return value of estimate_mystery can be thought of as the area of the unit circle / area of the square surrounding unit cicle
+#  or pi() / 4
