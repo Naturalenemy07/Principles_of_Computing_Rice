17e828ed1c213f781911f97efcee3dd1a2278404
John Caruthers <john.d.caruthers@gmail.com>
2021-01-17 00:35:16 -0500

    added functions

--- monte_carlo.py
+++ monte_carlo.py
@@ -15,6 +15,34 @@ SCORE_OTHER = 1.0   # Score for squares played by the other player
     
 # Add your functions here.
 
+# make a board 
+board = {}
+
+def mc_trial(board, player):
+"""
+Takes current board and the next player to move, makes random moves until game is finished
+"""
+    pass
+
+def mc_update_scores(scores, board, player):
+    """
+    takes a grid of scores, board of a completed game, and which player the machine is, 
+    it scores the completed board and update scores grid
+    """
+    pass
+
+def get_best_move(board, scores):
+    """
+    takes the current board(must contain empty squares) and the grid of scores,
+    finds maximum score and returns the one of them randomly
+    """
+
+def mc_move(board, player, trials):
+    """
+    takes the current board, which player machine is, and number of trials to run.
+    Uses monte carlo simulation to return move for machine player as a tuple
+    """
+
 
 
 # Test game with the console or the GUI.  Uncomment whichever 
