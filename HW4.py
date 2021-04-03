6e08a532d32d4e19f3c1b688bf98d3e38c317437
John Caruthers <john.caruthers1@astrazeneca.com>
2021-04-03 10:44:08 -0400

    resolved conflict

--- HW4.py
+++ HW4.py
@@ -1,4 +1,86 @@
+import math
+
+# Homework 4
+
+# Question 1
+def q1():
+    """
+    Coinflips that generate set of 5 length
+    """
+    # number of different sides and length
+    length = 5
+    sid = 2
+
+    # first set can be 2, next 2, next 2 and so on for length 5 or 2 ** 5
+    tot = sid ** length
+    print("Question 1: {}".format(tot))
+
+# Question 2
+# Consider a sequence of trials in which a fair four-sided die (with faces numbered 1-4) is rolled twice.  
+# What is the expected value of the product of the two die rolls? Enter the answer as a floating point number below.
+
+def q2():
+    """
+    Consider a sequence of trials in which a fair four-sided die (with faces numbered 1-4) is rolled twice.  
+    What is the expected value of the product of the two die rolls? Enter the answer as a floating point number below.
+    """
+    
+    def make_enum(outcomes, length):
+        """
+        Iterative function that enumerates the set of all sequences of
+        outcomes of given length
+        """
+        # I still dont understand this function
+        ans = set([()])
+        for dummy_idx in range(length):
+            temp = set()
+            for seq in ans:
+                for item in outcomes:
+                    new_seq = list(seq)
+                    new_seq.append(item)
+                    temp.add(tuple(new_seq))
+            ans = temp
+        return ans
 
 
+    def prod_lists_avg(list_list):
+        """
+        input::list_list: a iterable containing a set/list/tuple of enumerations
+        output: returns the average of the products of input enumerations
+        """
+        list_of_products = []
+        for lis in list_list:
+            product = 1
+            for ind in lis:
+                product *= ind
+            list_of_products.append(product)
+
+        running = 0.0
+        for prod in list_of_products:
+            running += prod
+        return running / len(list_of_products)
 
 
+    # set constants ()
+    sides = 4
+    die_sides = range(1, sides + 1)
+    rolls = 2
+
+    # first make the list of lists of all possible enumerations
+    poss_enum = make_enum(die_sides, rolls)
+
+    # next get the product  abd averageof all enumerations
+    print("Question 2: {}".format(prod_lists_avg(poss_enum)))
+
+
+# Question 3
+def q3():
+    """
+    Given a trial in which a decimal digit is selected from a list(0 - 9) with equal prob of 0.1.  Consider a 5-digit
+    string created by a sequence of such trials(0s and repeated digits allowed). What is the probability that this
+    5-string digit consists of five consecutive digits in ascending or descending order? 
+    """
+    print("Question 3: ")
+q1()
+q2()
+q3()
