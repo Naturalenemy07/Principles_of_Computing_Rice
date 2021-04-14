e6ab97c9992e00d6eabdf9ba32481ec3efa6326d
John Caruthers <john.d.caruthers@gmail.com>
2021-04-14 05:58:18 -0400

    Update HW4.py
    added question 9

--- HW4.py
+++ HW4.py
@@ -202,16 +202,48 @@ def q8():
     
     print("Qustion 8: {}".format(prob_suit))
     
+def q9():
+    """
+    Iterative program to print out Pascal's triangle
+    """
+
+    TRIANGLE_HEIGHT = 5
+
+    def next_line(current_line):
+        """
+        Given a line in Pascal's triangle, generate the following line
+        """
+
+        ans = [1]
+
+        for idx in range(len(current_line) - 1):
+            ans.append(current_line[idx] + current_line[idx + 1])
+
+        ans.append(1)
+
+        return ans
+
+    def run_example():
+        # code to print out Pascal's triangle
+        pascal_line = [1]	# row zero
+        print(pascal_line)
+
+        for dummy_idx in range(TRIANGLE_HEIGHT - 1):
+            pascal_line = next_line(pascal_line)
+            print(pascal_line)
+
+    run_example()
     
     
 #q1()
 #q2()
-q3()
+#q3()
 #q4()
 #q5()
 #q6()
 #q7()
-q8()
+#q8()
+q9()
     
 
 
